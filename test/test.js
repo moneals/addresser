c5420a72d5b23828bd8d59826d96543d3b0810d4
Michael Smith <michael@juniperbay.com>
2018-08-06 03:05:38 +0000

    Added test

--- test/test.js
+++ test/test.js
@@ -4,13 +4,18 @@ var expect = require('chai').expect;
 var addressParser = require('../index');
 
 describe('#addressParser', function() {
-    it('should convert single digits', function() {
+    it('should parse single digits', function() {
         var result = addressParser("1");
         expect(result).to.equal("parsed 1");
     });
 
-    it('should convert double digits', function() {
+    it('should parse double digits', function() {
         var result = addressParser("12");
         expect(result).to.equal("parsed 12");
+    });
+
+    it('should parse a string', function() {
+        var result = addressParser("mystring");
+        expect(result).to.equal("parsed mystring");
     });
 });
